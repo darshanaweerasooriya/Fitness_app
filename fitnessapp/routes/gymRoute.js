df9847119b55e7f5448aff7e95bd510da07b1a92
darshana weerasooriya <96476541+darshanaweerasooriya@users.noreply.github.com>
2025-08-31 01:06:54 +0530

    Fetching gym detail function is done

--- fitnessapp/routes/gymRoute.js
+++ fitnessapp/routes/gymRoute.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gymControll = require("../controllers/addGymController");
 
+
 router.post("/add",gymControll.addGym);
+router.get("/get",gymControll.getAllGyms);
 
 module.exports = router;
